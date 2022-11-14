b879b3db51aa080d851a7f810f44843daca834dd
Ricardo <richardproject.us@gmail.com>
2022-11-14 15:21:22 -0600

    switch on

--- bash.js
+++ bash.js
@@ -8,21 +8,18 @@ process.stdin.on('data', (data) => {
     const dataIn = data.toString().trim().split(" ");
     const cmd = dataIn[0];
     const fileName = dataIn[1];
-    if (cmd === 'pwd'){
-        pwd();
-    }
-    else if(cmd === 'ls'){
-        ls();
-    }
-    else if(cmd === 'cat'){
-        cat(fileName);
-  }
 
-
-  else{
-  process.stdout.write(`Command not found: ${cmd}`);
-  process.stdout.write('\n prompt > ');
-}
+    switch (cmd) {
+        case 'pwd': pwd();
+            break
+        case 'ls': ls();
+            break
+        case 'cat': cat(fileName);
+            break
+        default:
+            process.stdout.write(`Command not found: ${cmd}`),
+            process.stdout.write('\n prompt > ');
+    }
 
 })
 
