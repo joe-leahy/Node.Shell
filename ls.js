79d0a3bdc441585c1540c260f1a3d7217f29b774
joe-leahy <josleahy@icloud.com>
2022-11-14 14:40:02 -0500

    pwd and ls added

---
+++ ls.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+
+module.exports = function (){
+  // The stdin 'data' event fires after a user types in a line
+      fs.readdir('./', 'utf8', (err, files) => {
+        if (err) {
+          throw err;
+        } else {
+          process.stdout.write('\n')
+          process.stdout.write(files.join('\n'));
+          process.stdout.write('\n')
+          process.stdout.write('\nprompt > ');
+        }
+      });
+    }
---
+++ pwd.js
@@ -0,0 +1,12 @@
+module.exports = function (){
+// The stdin 'data' event fires after a user types in a line
+    process.stdout.write('\n')
+    process.stdout.write(process.cwd())
+    process.stdout.write('\n')
+    process.stdout.write('\nprompt > ');
+
+
+
+};
+
+
